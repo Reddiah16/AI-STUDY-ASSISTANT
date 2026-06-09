838dfe0c85bed334697d77af3ad712f386737812
Reddiah12 <reddiah151@gmail.com>
2026-06-09 15:04:20 +0530

    fix: refactor polyfill logic to separate src/polyfill.ts to clean main.tsx and resolve typescript target errors

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,4 @@
-// Polyfill Promise.withResolvers for environments/browsers lacking native support (e.g. older safari/chrome or test runner environments)
-if (typeof Promise.withResolvers === 'undefined') {
-  Promise.withResolvers = function <T>() {
-    let resolve!: (value: T | PromiseLike<T>) => void;
-    let reject!: (reason?: unknown) => void;
-    const promise = new Promise<T>((res, rej) => {
-      resolve = res;
-      reject = rej;
-    });
-    return { promise, resolve, reject };
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  } as any;
-}
+import './polyfill';
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
